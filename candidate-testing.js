f80bf1fd094d491aa65e21f663194589bc935996
faestreya <faestreya@gmail.com>
2024-06-20 15:31:09 -0500

    Passed every test but three, unsure of where I went wrong, please advise.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
@@ -18,12 +18,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+console.log("Please enter your name");
+candidateName = askForName
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(questions + "Please type your answer below:")
 
 }
 
@@ -31,7 +32,11 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-
+if (correctAnswer = true){
+  console.log("Correct!")
+} else {
+  console.log("Incorrect.")
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -42,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello," + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
